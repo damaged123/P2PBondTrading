7c2e632c1edea7562989767646368b4b6b734991
damaged123 <30801762+damaged123@users.noreply.github.com>
2020-05-11 10:39:33 +0900

    Update Core.py

--- Core.py
+++ Core.py
@@ -35,7 +35,7 @@ def RunAll():
     Eight_Per_Cent.FilterFromSubPage.get_missing_date(souptext)
     Eight_Per_Cent.FilterFromSubPage.find_P2P_debt(souptext)
 
-    InterestCost = Eight_Per_Cent.Debt_Calculation.Debt_Calculation(Debts,Credit) #Debt_Calculation.py모듈인식 불가
+    InterestCost = Eight_Per_Cent.Debt_Calculation.DebtAndCredit_Calculation(Debts,Credit) #Debt_Calculation.py모듈인식 불가
 
     Eight_Per_Cent.FilterFromSubPage.lonereq(souptext)
 
