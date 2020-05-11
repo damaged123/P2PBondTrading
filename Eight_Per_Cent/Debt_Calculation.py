a84643f3075ce17e9b144a30a41fd893a501ca8b
damaged123 <30801762+damaged123@users.noreply.github.com>
2020-05-11 10:55:37 +0900

    Update Debt_Calculation.py

--- Eight_Per_Cent/Debt_Calculation.py
+++ Eight_Per_Cent/Debt_Calculation.py
@@ -197,4 +197,64 @@ def InterestCost(Debts, Credit):
     
     return sum(NullList)
 
+ 
+#대출신청 정보 계산
+def lonereq(soup):
+    '''
+    세전 수익률, 상환기간, 모집금액(사람들이 넣은 돈), 대출금액(대출자가 모으고 싶은 돈)
+    '''
+    moneyList = []
+    
+    profit = soup.body.main.header.div.find_all('p')[3].text #수익률
+    profit = profit.replace('%', '')
+    profit = float(profit)
+    
+    period = soup.body.main.header.div.find_all('p')[5].text #상환기간
+    period = period.replace('개월', '')
+    period = int(period)
+    
+    
+    money = soup.body.main.header.div.find_all('p')[9].text #상환금액
+    money_gathering  = money.split('\n')[1].replace(' ', '') #모집금액(사람들이 넣은 돈)
+    money_gathering = int(money_gathering)
+    
+    money_gathered = money.split('\n')[3].replace(' ', '') #대출금액(대출자가 모으고 싶은 돈)
+    money_gathered = money_gathered.replace(',', '')
+    money_gathered = int(money_gathered)
+    
+    moneyList.append(profit) 
+    moneyList.append(period) 
+    moneyList.append(money_gathering) 
+    moneyList.append(money_gathered) 
+    return moneyList
 
+def NetIncome(test_text): #24%최대 이자로 일률적으로 적용하며 10년 만기로 설정한다. 추산이자채무상환액 = 원금/12*10(10년만기균등상환)+원금*3%이자(이자비용+ 쿠션1%)
+    #추가 대출금에 다한 반영X
+    '''
+    '''
+    Income = find_Income_index_in_string(test_text)[0]
+    Income = Income.replace('만원','')
+    Income = Income.replace(',','')
+    Income = int(Income)
+    Expense = find_Expense_index_in_string(test_text)[0]
+    Expense = Expense.replace('만원','')
+    Expense = Expense.replace(',','')
+    Expense = int(Expense)
+    
+    result = Income - (Expense)
+    return result
+
+def OverHead(test_text, soup):
+    gain = NetIncome(test_text)
+
+    Debts = find_debt_index_in_string(test_text)
+
+    Credit = find_Credit_index_in_string(test_text)
+
+    soup = crawl_investment_individual()
+
+    lose = Debt_Calculation(Debts, Credit) + (lonereq(soup)[-1]*(1+lonereq(soup)[0]/100))/lonereq(soup)[1]
+
+    result = gain - lose
+
+    return result
